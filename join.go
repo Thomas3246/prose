50f92059868a3dcba988b2ae84ee109b66e2825c
Thomas3246 <anatoliy0642751@mail.ru>
2024-08-05 16:43:22 +0300

    changes

--- join.go
+++ join.go
@@ -1,10 +1,10 @@
-package testing
+package prose
 
 import "strings"
 
 func JoinWinthCommas(phrases []string) string {
 	result := strings.Join(phrases[:len(phrases)-1], ", ")
-	result += " and "
+	result += ", and "
 	result += phrases[len(phrases)-1]
 	return result
 }
